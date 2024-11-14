cd1a85b374c4f67779167a1bf780d23317e10fd1
Eric Lopes Oliveira <olv.ericlopes@gmail.com>
2024-11-14 15:46:44 -0300

    lógica e template configurado de acordo com o requisito da atividade

--- app/src/main/java/com/example/atividade3/MainActivity.java
+++ app/src/main/java/com/example/atividade3/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.atividade3;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,68 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private View lightGreen, lightYellow, lightRed;
+    private TextView message;
+    private final Handler handler = new Handler();
+    private Runnable runnable;
+    private int state = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        lightGreen = findViewById(R.id.light_green);
+        lightYellow = findViewById(R.id.light_yellow);
+        lightRed = findViewById(R.id.light_red);
+        message = findViewById(R.id.message);
+        Button startButton = findViewById(R.id.start_button);
+
+        startButton.setOnClickListener(v -> startSemaphore());
+    }
+
+    private void startSemaphore()
+    {
+        runnable = new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                switch (state)
+                {
+                    case 0:
+                        lightGreen.setBackgroundColor(getResources().getColor(R.color.green));
+                        lightYellow.setBackgroundColor(getResources().getColor(R.color.black));
+                        lightRed.setBackgroundColor(getResources().getColor(R.color.black));
+                        message.setText("Pode seguir em frente!");
+                        state = 1;
+                        break;
+                    case 1:
+                        lightGreen.setBackgroundColor(getResources().getColor(R.color.black));
+                        lightYellow.setBackgroundColor(getResources().getColor(R.color.yellow));
+                        message.setText("ATENÇÃO! Vai fechar!");
+                        state = 2;
+                        break;
+                    case 2:
+                        lightYellow.setBackgroundColor(getResources().getColor(R.color.black));
+                        lightRed.setBackgroundColor(getResources().getColor(R.color.red));
+                        message.setText("PARE! Senão, leva multa!");
+                        state = 0;
+                        break;
+                }
+            handler.postDelayed(this, 3000);
+            }
+        };
+        handler.post(runnable);
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
     }
 }
